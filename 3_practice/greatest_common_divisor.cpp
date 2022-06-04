0a614f89501b04970f6fec889646b140744113b7
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2022-06-04 18:26:43 +0900

    greatest_common_devisor

--- 3_practice/greatest_common_divisor.cpp
+++ 3_practice/greatest_common_divisor.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 
+// O(2*min(A,B))
+// long long GCD(long long A, long long B){
+//   long long answer = 0;
+//   for (long long i = 1; i <= min(A, B); i++){
+//     if (A % i == 0 && B % i == 0) answer = i;
+//   }
+//   return answer;
+// }
+// O(log(A+B))に修正
 long long GCD(long long A, long long B){
-  long long answer = 0;
-  for (long long i = 1; i <= min(A, B); i++){
-    if (A % i == 0 && B % i == 0) answer = i;
+  while( A >= 1 && B >= 1){
+    if (A < B) B = B % A;
+    else A = A % B;
   }
-  return answer;
+  if (A >= 1) return A;
+  return B;
 }
 
 int main()
