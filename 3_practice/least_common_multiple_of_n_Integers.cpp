5119cd58ea874c1bdf7f7845ca0a0fbc8b4963f3
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2022-06-05 09:15:53 +0900

    Least Common Multiple of N Integers

--- 3_practice/least_common_multiple_of_n_Integers.cpp
+++ 3_practice/least_common_multiple_of_n_Integers.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//最大公約数
 long long GCD(long long A, long long B){
   while( A >= 1 && B >= 1){
     if (A < B) B = B % A;
@@ -10,24 +10,25 @@ long long GCD(long long A, long long B){
   return B;
 }
 
+// 最小公倍数
+long long LCM(long long A, long long B){
+  return (A / GCD(A,B)) * B;
+  
+  // return A * B / GCD(A,B);
+}
+
 int main()
 {
   long long N, A[100009];
   cin >> N;
   for (long long i = 1; i <= N; i++) cin >> A[i];
 
-  // この方法だと配列の中身を変えてしまうため、最小公倍数を計算するなどの応用ができない
-  // for (long long i = 1; i <= N - 1; i++){
-  //   A[i+1] = GCD(A[i],A[i+1]);
-  // }
-
-  long long R = GCD(A[1], A[2]);
+  long long R = LCM(A[1], A[2]);
   for (long long i = 3; i <= N ; i++){
-    // 最大公約数と次の数を比較していけばいい
-    R = GCD(R,A[i]);
+     // 最小公倍数と次の数を比較していけばいい
+    R = LCM(R,A[i]);
   }
-
-  // cout << A[N] << endl;
+  
   cout << R << endl;
   return 0;
 }
